0b24e747db89bedc5a3cbf532d6dcb485102b389
Isaac Vitorino Lopes Alves <129021004+ivtrL@users.noreply.github.com>
2024-08-02 19:30:42 -0300

    add new dependencies and frontend of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,107 @@
 import "./App.css";
+import { invoke } from "@tauri-apps/api";
+import { ArrowUpToLine, ClipboardCopy, FileInput, X } from "lucide-react";
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Separator } from "./components/separator";
+import { ProgressBar } from "./components/progress-bar";
+import { MenuItem } from "./components/menu-item";
 
-function App() {}
+function App() {
+  const [uploadQueue, setUploadQueue] = useState<File[]>([]);
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => handleStartUpload(acceptedFiles),
+    []
+  );
+
+  const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
+    onDrop,
+    multiple: true,
+    noClick: true,
+    noKeyboard: true,
+  });
+
+  console.log(isDragActive);
+
+  function handleStartUpload(files: File[]) {
+    setUploadQueue(files);
+  }
+
+  function handleQuit() {
+    invoke("quit_app");
+  }
+
+  function handleCancelUpload() {
+    setUploadQueue([]);
+  }
+
+  const status =
+    uploadQueue.length > 0 ? "accept" : isDragActive ? "active" : "pending";
+
+  return (
+    <div className="space-y-1">
+      <div
+        {...getRootProps()}
+        className="text-white/80 m-4 px-4 h-24 flex items-center border border-dashed rounded justify-center"
+      >
+        <input {...getInputProps()} />
+
+        {status === "active" && "Start upload..."}
+
+        {status === "accept" && (
+          <div className="flex flex-col items-center gap-3">
+            <div className="flex items-center gap-2 text-xs">
+              {uploadQueue.length > 1 ? (
+                <p className="animate-pulse">
+                  Uploading {uploadQueue.length} file(s)...
+                </p>
+              ) : (
+                <p className="animate-pulse">
+                  Uploading{" "}
+                  {uploadQueue[0].name.length > 14
+                    ? uploadQueue[0].name.substring(0, 14).concat("...")
+                    : uploadQueue[0].name}
+                </p>
+              )}
+
+              <button
+                onClick={handleCancelUpload}
+                title="Cancel upload"
+                className="text-red-300"
+              >
+                <X className="w-4 h-4" />
+              </button>
+            </div>
+
+            <ProgressBar progress={40} />
+          </div>
+        )}
+
+        {status === "pending" && (
+          <div className="flex items-center gap-3">
+            <ArrowUpToLine className="w-4 h-4" />
+            <p>Drag files here...</p>
+          </div>
+        )}
+      </div>
+      <Separator />
+      <nav className="px-1.5 w-full">
+        <MenuItem onClick={open} hotkey="mod+o">
+          <FileInput className="w-4 h-4 stroke-[1.5px]" />
+          Select file
+        </MenuItem>
+
+        <MenuItem hotkey="mod+shift+v">
+          <ClipboardCopy className="w-4 h-4 stroke-[1.5px]" />
+          Upload from clipboard
+        </MenuItem>
+        <Separator />
+        <MenuItem hotkey="mod+q" onClick={handleQuit}>
+          Quit
+        </MenuItem>
+      </nav>
+    </div>
+  );
+}
 
 export default App;
---
+++ src/components/menu-item.tsx
@@ -0,0 +1,70 @@
+import { ComponentProps, ElementRef, useRef } from "react";
+import { twMerge } from "tailwind-merge";
+import { useHotkeys } from "react-hotkeys-hook";
+
+interface MenuItemProps extends ComponentProps<"button"> {
+  hotkey?: string;
+}
+
+function getSymbolCombination(hotkey: string) {
+  const modifierMap: Record<string, string> = {
+    shift: "⇧",
+    ctrl: "^",
+    alt: "⌥",
+    meta: "⌘",
+    mod: isMacOS() ? "⌘" : "^",
+  };
+
+  const keys = hotkey.split("+");
+
+  const symbolModifiers = keys
+    .map((key) => {
+      return key in modifierMap ? modifierMap[key] : key.toLocaleUpperCase();
+    })
+    .join("");
+
+  return symbolModifiers;
+}
+
+function isMacOS() {
+  return navigator.userAgent.toUpperCase().includes("MAC");
+}
+
+export function MenuItem({
+  hotkey = "",
+  children,
+  className,
+  ...props
+}: MenuItemProps) {
+  const buttonRef = useRef<ElementRef<"button">>(null);
+
+  useHotkeys(
+    hotkey,
+    () => {
+      const clickEvent = new MouseEvent("click", {
+        view: window,
+        bubbles: true,
+      });
+
+      buttonRef.current?.dispatchEvent(clickEvent);
+    },
+    {
+      enabled: hotkey !== "",
+      preventDefault: true,
+    }
+  );
+
+  return (
+    <button
+      ref={buttonRef}
+      className={twMerge(
+        "flex group px-2.5 py-1.5 cursor-default items-center rounded-sm gap-2 hover:bg-slate-600/90 mt-2 w-full justify-between text-white text-sm",
+        className
+      )}
+      {...props}
+    >
+      <div className="flex gap-2 items-center">{children}</div>
+      <div className="self-end">{hotkey && getSymbolCombination(hotkey)}</div>
+    </button>
+  );
+}
